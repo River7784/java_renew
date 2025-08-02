0d2577d94dee1d96d277853f16ad6509aa1996ff
River7784 <119822951+River7784@users.noreply.github.com>
2025-08-02 11:23:22 +0900

    Add files via upload

---
+++ ex_04.java
@@ -0,0 +1,8 @@
+public class ex_04 {
+    public static void main(String[] args){
+        String hobby = "";
+        if(hobby.equals("")){
+            System.out.println("hobby 변수가 참조는 String 객체는 빈 문자열");
+        }
+    }
+}

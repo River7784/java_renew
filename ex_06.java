9353040491071e2daeec8482dfd6df980d772d38
River7784 <119822951+River7784@users.noreply.github.com>
2025-08-04 21:52:08 +0900

    Add files via upload

---
+++ ex_06.java
@@ -0,0 +1,11 @@
+public class ex_06 {
+    public static void main(String[] args){
+        String ssn = "9710271234123";
+        int length = ssn.length();
+        if (length == 13){
+            System.out.println("주민등록번호 자릿수가 맞습니다.");
+        }else {
+            System.out.println("주민등록번호 자릿수가 틀립니다.");
+        }
+    }
+}

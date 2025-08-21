85cd1e3e2c990b171e1528f109dbee8ecd1e0bfe
River7784 <119822951+River7784@users.noreply.github.com>
2025-08-21 22:27:03 +0900

    Add files via upload

---
+++ ex_22.java
@@ -0,0 +1,9 @@
+public class ex_22 {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        System.out.println("s1 변수가 Student 객체를 참조함");
+
+        Student s2 = new Student();
+        System.out.println("s2 변수가 또 다른 Student 객체를 참조함");
+    }
+}

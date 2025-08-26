820304e8d01e9976979df007b8ddbae1f9b77ae6
River7784 <119822951+River7784@users.noreply.github.com>
2025-08-26 22:28:21 +0900

    Add files via upload

--- car_spec.java
+++ car_spec.java
@@ -5,4 +5,29 @@ public class car_spec {
     String color = "검정";
     int hp = 200;
     int torque;
+
+    //생성자 선언
+    car_spec(){}
+
+    car_spec(String model){
+        this.model = model;
+    }
+
+    car_spec(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    car_spec(String model, String color, int hp){
+        this.model = model;
+        this.color = color;
+        this.hp = hp;
+    }
+
+    car_spec(String model, String color, int hp, int torque){
+        this.model = model;
+        this.color = color;
+        this.hp = hp;
+        this.torque = torque;
+    }
 }

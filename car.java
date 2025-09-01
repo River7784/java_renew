a00c3acabbdf3fa79842407db4ebb762dc70edea
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-01 21:16:59 +0900

    Add files via upload

--- car.java
+++ car.java
@@ -2,4 +2,17 @@ public class car {
     String model;
     boolean start;
     int speed;
+
+    car(String model){
+        this.model = model;
+    }
+
+    void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    void run() {
+        this.setSpeed(100);
+        System.out.println(this.model + "의 속도는 시속 " + this.speed + "km/h");
+    }
 }

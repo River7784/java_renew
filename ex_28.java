e821beb639ce38b8b404c6e5bbe413a683c880d5
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-02 21:38:12 +0900

    Add files via upload

---
+++ ex_28.java
@@ -0,0 +1,9 @@
+public class ex_28 {
+    public static void main(String[] args){
+        car myCar = new car("benz");
+        car yourCar = new car("bmw");
+
+        myCar.run();
+        yourCar.run();
+    }
+}

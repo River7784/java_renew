7f5e8c8e2aae689d204e2b3561e4c7617485d947
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-11 21:38:54 +0900

    Add files via upload

---
+++ ex_32.java
@@ -0,0 +1,11 @@
+public class ex_32 {
+    int speed;
+
+    void run() {}
+
+    public static void main(String[] args){
+        Car myCar = new Car();
+        myCar.speed = 60;
+        myCar.run();
+    }
+}

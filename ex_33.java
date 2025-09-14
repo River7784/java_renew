a3513d779cdac6780fec2238a963ff9f94f8706c
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-14 21:27:34 +0900

    Add files via upload

---
+++ ex_33.java
@@ -0,0 +1,9 @@
+public class ex_33 {
+    static final double EARTH_RADIUS = 6400;
+    
+    static double EARTH_SURFACE_AREA = 0;
+    
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+    }
+}

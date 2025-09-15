829d16ea74c3eb70bbccf115d15cb968cb84a205
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-15 19:55:19 +0900

    Add files via upload

---
+++ ex_34.java
@@ -0,0 +1,13 @@
+public class ex_34 {
+    A a1 = new A(true);
+    A a2 = new A(1);
+    A a3 = new A("STRING");
+
+    public  A(boolean b){
+    }
+
+    A(int b);
+
+    private A(String s){
+    }
+}

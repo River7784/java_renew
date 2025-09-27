9cce181d1590168d547f37651c7dd398d78f2465
River7784 <119822951+River7784@users.noreply.github.com>
2025-09-27 16:11:26 +0900

    Add files via upload

---
+++ ex_44.java
@@ -0,0 +1,10 @@
+public class ex_44 {
+    public String model;
+    public String color;
+
+    public ex_44(String model, String color){
+        this.model = model;
+        this.color = color;
+        System.out.println("Phone(String model, String color) 생성자 실행");
+    }
+}

de879d056e65dfdffe5a9a12f178c186a5e783d7
River7784 <119822951+River7784@users.noreply.github.com>
2025-10-02 23:18:31 +0900

    Add files via upload

---
+++ airplane.java
@@ -0,0 +1,11 @@
+public class airplane {
+    public void land(){
+        System.out.println("착륙");
+    }
+    public void fly(){
+        System.out.println("비행");
+    }
+    public void takeoff(){
+        System.out.println("이륙");
+    }
+}

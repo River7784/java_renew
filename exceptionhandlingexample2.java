1872042e1e54f3b4fbee62cc5ba46edc749c04ca
River7784 <119822951+River7784@users.noreply.github.com>
2025-10-13 19:37:58 +0900

    Add files via upload

---
+++ exceptionhandlingexample2.java
@@ -0,0 +1,18 @@
+public class exceptionhandlingexample2 {
+    public static void printLength(String data){
+        try {
+            int result = data.length();
+            System.out.println("문자의 수 : " + result);
+        } catch (NullPointerException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("마무리 실행\n");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("프로그램 시작 \n");
+        printLength("ThisIsJava");
+        printLength(null);
+        System.out.println("프로그램 종료");
+    }
+}

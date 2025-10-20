8a6c90dbc1dbfe8e69ab4ef847970cab077623ab
River7784 <119822951+River7784@users.noreply.github.com>
2025-10-21 00:10:15 +0900

    Add files via upload

---
+++ throwexample2.java
@@ -0,0 +1,9 @@
+public class throwexample2 {
+    public static void main(String[] args) throws Exception{
+        findClass();
+    }
+
+    public static void findClass() throws  ClassNotFoundException{
+        Class.forName("java.lang.String2");
+    }
+}
